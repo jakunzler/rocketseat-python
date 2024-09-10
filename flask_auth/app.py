3c6a0eba02c286c99b4a3f9d2851663e5984750e
Jonas Augusto Kunzler <k.jonasaugusto@gmail.com>
2024-09-10 14:50:35 -0300

    update: implements user flux

--- flask_auth/app.py
+++ flask_auth/app.py
@@ -11,6 +11,12 @@ login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
 
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
@@ -19,8 +25,65 @@ def login():
     user = User.query.filter_by(username=username).first()
     if user and user.password == password:
         login_user(user)
+        print(current_user.is_authenticated)
         return jsonify({'message': 'Logged in successfully!'})
     return jsonify({'message': 'Invalid credentials!'})
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Logged out successfully!'})
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    family_name = data.get('family_name')
+    given_name = data.get('given_name')
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+    
+    if username and password:
+        new_user = User(family_name=family_name, given_name=given_name, username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'User registered successfully!'})
+    else:
+        return jsonify({'message': 'Username and password are required!'}), 400
+
+@app.route('/user/<int:id_user>', methods=['GET'])
+@login_required
+def get_user(id_user):
+    user = User.query.get_or_404(id_user)
+    return jsonify(user.to_dict())
+
+@app.route('/user/<int:id_user>', methods=['PUT'])
+@login_required
+def update_user(id_user):
+    data = request.get_json()
+    user = User.query.get_or_404(id_user)
+    
+    user.family_name = data.get('family_name', user.family_name)
+    user.given_name = data.get('given_name', user.given_name)
+    user.username = data.get('username', user.username)
+    user.email = data.get('email', user.email)
+    user.password = data.get('password', user.password)
+    
+    db.session.commit()
+    return jsonify({'message': 'User updated successfully!'})
+
+@app.route('/user/<int:id_user>', methods=['DELETE'])
+@login_required
+def delete_user(id_user):
+    user = User.query.get_or_404(id_user)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message': 'User deleted successfully!'})
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'message': 'Resource not found!'}), 404
 
 @app.route('/', methods=['GET'])
 def home():
--- flask_auth/models/user.py
+++ flask_auth/models/user.py
@@ -1,10 +1,10 @@
-from app import db
+from database import db
 from sqlalchemy.dialects.postgresql import UUID
 from flask_login import UserMixin
 
-class User(db.Model):
-    # id = db.Column(UUID(as_uuid=True), primary_key=True, server_default=db.text("uuid_generate_v4()"))
-    id = db.Column(db.Integer, primary_key=True)
+class User(db.Model, UserMixin):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, server_default=db.text("uuid_generate_v4()"))
+    # id = db.Column(db.Integer, primary_key=True)
     family_name = db.Column(db.String(80), unique=False, nullable=False)
     given_name = db.Column(db.String(120), unique=False, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -12,5 +12,12 @@ class User(db.Model):
     password = db.Column(db.String(120), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
     
-    # def __repr__(self):
-    #     return f'<User {self.username}>'
+    # def to_dict(self):
+    #     return {
+    #         'id': self.id,
+    #         'family_name': self.family_name,
+    #         'given_name': self.given_name,
+    #         'username': self.username,
+    #         'email': self.email,
+    #         'is_admin': self.is_admin
+    #     }
