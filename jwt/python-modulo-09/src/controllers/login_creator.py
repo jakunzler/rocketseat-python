aad4280800c6fad4244e9a4445daaade0359923d
Jonas Kunzler <k.jonasaugusto@gmail.com>
2024-10-03 11:37:17 -0300

    fix: update instructor code by creating errors folder

--- jwt/python-modulo-09/src/controllers/login_creator.py
+++ jwt/python-modulo-09/src/controllers/login_creator.py
@@ -2,6 +2,8 @@ from typing import Dict, Tuple
 from src.models.interface.user_repository import UserRepositoryInterface
 from src.drivers.jwt_handler import JwtHandler
 from src.drivers.password_handler import PasswordHandler
+from src.errors.types.http_bad_request import HttpBadRequestError
+from src.errors.types.http_not_found import HttpNotFoundError
 from .interfaces.login_creator import LoginCreatorInterface
 
 
@@ -22,13 +24,13 @@ class LoginCreator(LoginCreatorInterface):
 
     def __find_user(self, username: str) -> Tuple[int, str, str]:
         user = self.__user_repository.get_user_by_username(username)
-        if not user: raise Exception("User not found")
+        if not user: raise HttpNotFoundError("User not found")
 
         return user
 
     def __verify_correct_password(self, password: str, hashed_password: str) -> None:
         is_password_correct = self.__password_handle.check_password(password, hashed_password)
-        if not is_password_correct: raise Exception("Wrong Password")
+        if not is_password_correct: raise HttpBadRequestError("Wrong Password")
 
     def __create_jwt_token(self, user_id: int) -> str:
         payload = { "user_id": user_id }
