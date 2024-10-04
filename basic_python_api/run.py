314ea29c9d6b3e54d176689c25907bcd13f6524d
Jonas Augusto Kunzler <k.jonasaugusto@gmail.com>
2024-10-04 12:13:16 -0300

    fix: run and server files

--- basic_python_api/run.py
+++ basic_python_api/run.py
@@ -1,6 +1,4 @@
-from app import create_app
-
-app = create_app()
+from src.app.server.server import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
